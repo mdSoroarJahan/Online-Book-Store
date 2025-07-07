1ee39855c17fae10810905bc9fe5894d8a2861c6
mdSoroarJahan <soroarjahan17@gmail.com>
2025-07-08 03:25:17 +0600

    Adding updateBook.php

--- api/view/books/updateBook.php
+++ api/view/books/updateBook.php
@@ -3,3 +3,24 @@ require_once '../../config/dbConfig.php';
 require_once '../../classes/books.php';
 
 header("Content-Type: application/json");
+$dbObj = new Database();
+$databaseConnection = $dbObj->getConnection();
+
+$booksObj = new Books($databaseConnection);
+
+if (isset($_POST['book_id'], $_POST['title'], $_POST['price'], $_POST['stock_qty'], $_POST['author_id'])) {
+    $book_id = $_POST['book_id'];
+    $title = $_POST['title'];
+    $price = $_POST['price'];
+    $stock = $_POST['stock_qty'];
+    $author_id = $_POST['author_id'];
+
+    $updateResult = $booksObj->updateBook($book_id, $title, $price, $stock, $author_id);
+    if ($updateResult) {
+        echo json_encode(["message" => "Book updated successfully"]);
+    } else {
+        echo json_encode(["message" => "Failed to update book"]);
+    }
+} else {
+    echo json_encode(["message" => "All fields are required"]);
+}
