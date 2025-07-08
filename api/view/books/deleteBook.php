380a33d5e56e11b53b8bef5dc13c75b30e7a5007
mdSoroarJahan <soroarjahan17@gmail.com>
2025-07-08 15:12:56 +0600

    Complete API and added some headers

--- api/view/books/deleteBook.php
+++ api/view/books/deleteBook.php
@@ -4,6 +4,10 @@ require_once '../../classes/books.php';
 
 header("Content-Type: application/json");
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $dbObj = new Database();
 $databaseConnection = $dbObj->getConnection();
 
--- api/view/books/updateBook.php
+++ api/view/books/updateBook.php
@@ -3,6 +3,11 @@ require_once '../../config/dbConfig.php';
 require_once '../../classes/books.php';
 
 header("Content-Type: application/json");
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 $dbObj = new Database();
 $databaseConnection = $dbObj->getConnection();
 
